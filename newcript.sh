856b1637871608d2131727cb60408010b26635ec
Anastasiia Zaleska <zaleska.anastasiia@gmail.com>
2023-08-16 17:09:42 +0300

    First version of the project

---
+++ newcript.sh
@@ -0,0 +1,1 @@
+"mcdir newDir; echo My cyrrent Directory is $PWD and I created a new one clled nediDir; echo take a look at the current directory content:; dir;" 
---
+++ newscript.sh
@@ -0,0 +1,4 @@
+mkdir newDir;
+echo My Current Directory is $PWD and I created a new one called nediDir;
+echo take a look at the current directory content:;
+dir;
